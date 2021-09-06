c02f8db3aeaada8d0baffe4634243eb58427b65f
ArtyomGorbackij <artyom_gorbaczkij@mail.ru>
2021-09-06 19:24:26 +0300

    Initial Commit

---
+++ Test_app/Test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_appApp.swift
+//  Test_app
+//
+//  Created by Артем on 06.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
