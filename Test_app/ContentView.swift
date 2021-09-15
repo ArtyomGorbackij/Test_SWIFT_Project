39218edb4a8ba1a28b9f1a7f41354140a85bae8d
ArtyomGorbackij <artyom_gorbaczkij@mail.ru>
2021-09-15 20:42:47 +0300

    Sha implemented

--- Test_app/ContentView.swift
+++ Test_app/ContentView.swift
@@ -14,8 +14,6 @@ struct ContentView: View {
     @State var username: String = ""
     @State var password: String = ""
     
-    @State var concantenate_of_strings: String = ""
-    
     var body: some View {
         VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/){
                 usernameField(username: $username)
@@ -25,8 +23,10 @@ struct ContentView: View {
                 passwordField(password: $password)
                     .textContentType(.postalCode)
                 Button(action: {
-                    self.concantenate_of_strings += uuid + self.username + self.password
+                    let concantenate_of_strings = self.password + self.username + uuid
                     print( concantenate_of_strings )
+                    let hashed = concantenate_of_strings.sha256()
+                    print(hashed)
                 }) {
                         Text("Button")
                     }
@@ -61,5 +61,42 @@ struct passwordField: View {
     var body: some View {
         SecureField("Password", text: $password)
             .padding(.all)
+    }
+}
+
+import Foundation
+import CommonCrypto
+
+extension Data{
+    public func sha256() -> String{
+        return hexStringFromData(input: digest(input: self as NSData))
+    }
+    
+    private func digest(input : NSData) -> NSData {
+        let digestLength = Int(CC_SHA256_DIGEST_LENGTH)
+        var hash = [UInt8](repeating: 0, count: digestLength)
+        CC_SHA256(input.bytes, UInt32(input.length), &hash)
+        return NSData(bytes: hash, length: digestLength)
+    }
+    
+    private  func hexStringFromData(input: NSData) -> String {
+        var bytes = [UInt8](repeating: 0, count: input.length)
+        input.getBytes(&bytes, length: input.length)
+        
+        var hexString = ""
+        for byte in bytes {
+            hexString += String(format:"%02x", UInt8(byte))
+        }
+        
+        return hexString
+    }
+}
+
+public extension String {
+    func sha256() -> String{
+        if let stringData = self.data(using: String.Encoding.utf8) {
+            return stringData.sha256()
+        }
+        return ""
     }
 }
